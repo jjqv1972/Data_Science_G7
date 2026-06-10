b4f3e7a6e3e0c3b89433874012a70dceb16806b4
Javier José Quispe Varillas <jquispe@serfor.gob.pe>
2026-06-10 16:22:15 -0500

    modulo8dia2f

--- modulo8/dia2/flask/primer_app.py
+++ modulo8/dia2/flask/primer_app.py
@@ -17,5 +17,22 @@ def sumar(a,b):
     resultado = a + b
     return f"<center><h1>La suma de {a} + {b} es {resultado} </h1></center>"
 
+@app.route('/<operacion>/<int:num1>/<int:num2>')
+def operaciones(operacion,num1,num2):
+    if operacion=="suma":
+        res = num1 + num2
+    elif operacion=="resta":
+        res= num1 - num2
+    elif operacion=="multiplicacion":
+        res= num1 * num2
+    elif operacion=="division":
+        if num2==0:
+            return "<h1>Error: División por cero no permitida</h1>"
+        res= float(num1 / num2)        
+    else:
+        return "<h1>Error: Operación no válida</h1>"
+        
+    return f"<h1>La {operacion} de {num1} y {num2} es {res}</h1>"
+
 
 app.run(debug=True)
