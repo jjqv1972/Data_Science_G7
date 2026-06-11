5bb6a5d3e96e3cfd0b3d63385537f9ed9f12c681
Javier José Quispe Varillas <jquispe@serfor.gob.pe>
2026-06-11 09:56:51 -0500

    modulo8v4

--- modulo8/dia2/housing-web/app.py
+++ modulo8/dia2/housing-web/app.py
@@ -1,10 +1,25 @@
 from flask import Flask,request,render_template
+import joblib
+import numpy as np
+import sklearn
+
+model = joblib.load('./model/model.pkl')
+sc_x = joblib.load('./model/scaler_x.pkl')
+sc_y = joblib.load('./model/scaler_y.pkl')
 
 app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    precio = 0
+    if request.method=="POST":
+        rooms = int(request.form['rooms'])
+        rooms_sc = sc_x.transform(np.array([[rooms]]))
+        prediction_sc = model.predict(rooms_sc)
+        prediction = sc_y.inverse_transform(prediction_sc) * 10000
+        precio = round(prediction[0][0],2)
+           
+    return render_template('index.html',precio=precio)
 
 if __name__ == "__main__":
     app.run(debug=True)
