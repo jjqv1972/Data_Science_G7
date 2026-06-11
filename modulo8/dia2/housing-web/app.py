5f2ad2a1daf36f57ab2fe503b29e3f77b0214960
Javier José Quispe Varillas <jquispe@serfor.gob.pe>
2026-06-11 11:05:39 -0500

    modulo8v5

--- modulo8/dia2/housing-web/app.py
+++ modulo8/dia2/housing-web/app.py
@@ -12,14 +12,15 @@ app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def index():
     precio = 0
+    rooms = 1
     if request.method=="POST":
         rooms = int(request.form['rooms'])
         rooms_sc = sc_x.transform(np.array([[rooms]]))
         prediction_sc = model.predict(rooms_sc)
         prediction = sc_y.inverse_transform(prediction_sc) * 10000
-        precio = round(prediction[0][0],2)
+        precio = abs(round(prediction[0][0],2))
            
-    return render_template('index.html',precio=precio)
+    return render_template('index.html',precio=precio,rooms=rooms)
 
 if __name__ == "__main__":
     app.run(debug=True)
