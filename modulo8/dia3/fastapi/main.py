f004ea04c178e677f9b2382ad56006798c070e9f
Javier José Quispe Varillas <jquispe@serfor.gob.pe>
2026-06-11 18:38:42 -0500

    modulo8e

--- modulo8/dia3/fastapi/main.py
+++ modulo8/dia3/fastapi/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
 @app.get("/")
@@ -23,4 +24,36 @@ def listar_productos(categoria: str="todos", precio_min: float=0):
             {"id":1, "nombre":"Laptop","precio":3500},
             {"id":2, "nombre":"Mouse","precio":80}
         ]        
+    }
+
+# Validacion conb Pydantic
+#sin validacion
+@app.post("/productos")
+def crear_producto(data: dict):
+    nombre = data.get("nombre")
+    precio = data.get("precio")
+    
+    return {
+        "nombre":nombre,
+        "precio":precio
+    }
+    
+#con validacion
+from pydantic import BaseModel, Field
+
+class Producto(BaseModel):
+    nombre: str = Field(
+        min_length = 3,
+        max_length = 50
+    )
+    
+    precio: float = Field(
+        gt=0
+    )
+    
+@app.post("/productosval")
+def crear_producto_validado(producto: Producto):
+    return{
+        "nombre": producto.nombre,
+        "precio": producto.precio
     }
